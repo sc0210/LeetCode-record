bfcf6d5298bb041726c6170bf4ca82d5a249d9f5
sc0210 <scdev0210@gmail.com>
2025-08-03 17:36:20 +0800

    feat: update string solution

--- source/0028_Find_the_Index_of_the_First_Occurrence_in_a_String.cc
+++ source/0028_Find_the_Index_of_the_First_Occurrence_in_a_String.cc
@@ -9,12 +9,7 @@ public:
     int strStr(string haystack, string needle)
     {
         // method1: string find API
-        std::size_t found = haystack.find(needle);
-        if (found == string::npos) {
-            return -1;
-        } else {
-            return found;
-        }
+        return haystack.find(needle);
 
         // method2: fixed size window comparsion
         for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
